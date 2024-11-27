80ff158bb26a1b8237e521f946aa6e18c44e0e6c
Mansur Uralov <m.uralov@sap.com>
2024-11-27 05:49:18 +0100

    Improve RAG and its tests

--- tests/integration/rag/test_rag_system.py
+++ tests/integration/rag/test_rag_system.py
@@ -5,8 +5,8 @@ from deepeval import evaluate
 from deepeval.metrics import (
     AnswerRelevancyMetric,
     ContextualRecallMetric,
-    FaithfulnessMetric,
     ContextualRelevancyMetric,
+    FaithfulnessMetric,
 )
 from deepeval.test_case import LLMTestCase
 
@@ -54,7 +54,7 @@ def rag_system(app_models):
 
 
 @pytest.mark.parametrize(
-    "input, expected_output_path",
+    "user_query, expected_output_path",
     [
         pytest.param(
             "How to enable Istio sidecar proxy injection?",
@@ -62,19 +62,28 @@ def rag_system(app_models):
             id="How to enable Istio sidecar proxy injection?",
         ),
         pytest.param(
-            "Why do I get a 'Connection reset by peer' response error?",
+            "Why do I get a 'Connection reset by peer' error?",
+            # "Why do I get a connection refuses error?",
             "fixtures/kyma_docs/istio/docs/user/troubleshooting/03-20-connection-refused.md",
-            id="Why do I get a Connection reset by peer error?",
+            id="Why do I get a 'Connection reset by peer' error?",
+        ),
+        pytest.param(
+            "how to resolve 'Connection reset by peer' error?",
+            # "Why do I get a connection refuses error?",
+            "fixtures/kyma_docs/istio/docs/user/troubleshooting/03-20-connection-refused.md",
+            id="how to resolve 'Connection reset by peer' error?",
         ),
         pytest.param(
             "function pod have have no sidecar proxy",
-            "fixtures/kyma_docs/istio/docs/user/troubleshooting/03-30-istio-no-sidecar.md",
+            # "fixtures/kyma_docs/istio/docs/user/troubleshooting/03-30-istio-no-sidecar.md",
+            "fixtures/kyma_docs/istio/docs/user/troubleshooting/no-sidecar-proxy.md",
             id="Pods don't have sidecar",
         ),
         # serverless
         pytest.param(
             "How to expose a Function Using the APIRule Custom Resource?",
-            "fixtures/kyma_docs/serverless/docs/user/tutorials/01-20-expose-function.md",
+            # "fixtures/kyma_docs/serverless/docs/user/tutorials/01-20-expose-function.md",
+            "fixtures/kyma_docs/serverless/docs/user/tutorials/expose-function-api-rule.md",
             id="How to expose a Function Using the APIRule Custom Resource?",
         ),
         pytest.param(
@@ -90,6 +99,7 @@ def rag_system(app_models):
         pytest.param(
             "adding a new env var to a function",
             "fixtures/kyma_docs/serverless/docs/user/tutorials/01-120-inject-envs.md",
+            # "fixtures/kyma_docs/serverless/docs/user/tutorials/inject-function-env-var.md",
             id="adding a new env var to a function",
         ),
         pytest.param(
@@ -105,40 +115,49 @@ def rag_system(app_models):
         # telemetry manager
         pytest.param(
             "show how to create a trace pipeline",
-            "fixtures/kyma_docs/telemetry-manager/docs/user/resources/04-tracepipeline.md",
+            "fixtures/kyma_docs/telemetry-manager/docs/user/creating-trace-pipeline.md",
             id="show how to create a trace pipeline",
         ),
-        # # TODO: enable it after indexing is improved. Currently it is failing.
+        # TODO: enable it after indexing is improved. Currently it is failing.
+        # "fixtures/kyma_docs/telemetry-manager/docs/user/02-logs.md"
         pytest.param(
             "what are the prerequisites for Kyma application to enable logging?",
             "fixtures/kyma_docs/telemetry-manager/docs/user/app-log-prerequisites.md",
             id="what are the prerequisites for applications to enable logging?",
         ),
+        pytest.param(
+            "why there is no logs in the backend?",
+            "fixtures/kyma_docs/telemetry-manager/docs/user/no-logs.md",
+            id="why there is no logs in the backend?",
+        ),
         # # eventing
+        # "fixtures/kyma_docs/eventing-manager/docs/user/troubleshooting/evnt-05-fix-pending-messages.md",
         pytest.param(
             "some eventing messages are pending in the stream",
-            # "fixtures/kyma_docs/eventing-manager/docs/user/troubleshooting/evnt-05-fix-pending-messages.md",
             "fixtures/kyma_docs/eventing-manager/docs/user/troubleshooting/pending-events.md",
             id="some eventing messages are pending",
         ),
+        # "fixtures/kyma_docs/eventing-manager/docs/user/troubleshooting/evnt-04-free-jetstream-storage.md",
         pytest.param(
             "what to do if event publish rate is so high?",
             "fixtures/kyma_docs/eventing-manager/docs/user/troubleshooting/high-event-rate.md",
-            # "fixtures/kyma_docs/eventing-manager/docs/user/troubleshooting/evnt-04-free-jetstream-storage.md",
             id="what to do if event publish rate is so high?",
         ),
     ],
 )
-def test_rag_search(input, expected_output_path, rag_system, evaluation_metrics):
+@pytest.mark.asyncio
+async def test_rag_search(
+    user_query, expected_output_path, rag_system, evaluation_metrics
+):
     # getting rag dir path
     rag_dir = os.path.dirname(os.path.abspath(__file__))
 
     # When
-    query = Query(text=input)
-    retrieved_docs = rag_system.retrieve(query, 20)
+    query = Query(text=user_query)
+    retrieved_docs = await rag_system.aretrieve(query)
     assert len(retrieved_docs) > 0, "No documents retrieved"
 
-    actual_output = rag_system.generate(query, retrieved_docs)
+    actual_output = await rag_system.agenerate(query, retrieved_docs)
     assert actual_output is not None, "RAG system generated no output"
 
     with open(os.path.join(rag_dir, expected_output_path)) as file:
@@ -149,7 +168,7 @@ def test_rag_search(input, expected_output_path, rag_system, evaluation_metrics)
     retrieved_docs_content = [doc.page_content for doc in retrieved_docs]
 
     test_case = LLMTestCase(
-        input=input,
+        input=user_query,
         actual_output=actual_output,
         retrieval_context=retrieved_docs_content,
         expected_output=expected_output,
